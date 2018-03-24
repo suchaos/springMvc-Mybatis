a24cef2ea8ef838aef8e6f7c86fdbc5b38e9eebc
suchaos <822075222@qq.com>
2018-03-24 23:16:08 +0800

    第三章学习

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # springMvc-Mybatis
 Spirng Mvc + Mybatis 学习
+
+问题记录：
+了解 log4j 配置文件的加载
+sqlSession.commit(); 数据库存储过程 commit close rollback 等
+
+知识点记录：
+mybatis insert 返回 id：
+````
+<insert id="insertUser" parameterType="com.suchaos.po.User">
+    <selectKey keyProperty="id" order="AFTER" resultType="java.lang.Integer">
+        SELECT LAST_INSERT_ID()
+    </selectKey>
+    INSERT INTO user(username, password, gender, email, province, city, birthday)
+    VALUE (#{username}, #{password}, #{gender}, #{email}, #{province}, #{city}, #{birthday, jdbcType=DATE})
+</insert>
+
+<insert id="insertUser" parameterType="com.suchaos.po.User" useGeneratedKeys="true" keyProperty="id">
+    INSERT INTO user(username, password, gender, email, province, city, birthday)
+    VALUE (#{username}, #{password}, #{gender}, #{email}, #{province}, #{city}, #{birthday, jdbcType=DATE})
+</insert>
+````
--- spirngmvcmybatis/src/test/java/com/suchaos/MyBatisTest.java
+++ spirngmvcmybatis/src/test/java/com/suchaos/MyBatisTest.java
@@ -9,7 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class MyBatisTest {
     private SqlSession sqlSession;
@@ -30,6 +32,59 @@ public class MyBatisTest {
         System.out.println("生日:" + sdf.format(user.getBirthday()));
         System.out.println("所在地:" + user.getProvince() + user.getCity());
     }
+
+    @Test
+    public void TestInsert() throws IOException, ParseException {
+        User user = new User();
+        user.setUsername("suchao");
+        user.setGender("male");
+        user.setPassword("5555");
+        user.setEmail("5555@126.com");
+        user.setProvince("222");
+        user.setCity("22");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        user.setBirthday(sdf.parse("1994-05-22"));
+        int id;
+        sqlSession.insert("test.insertUser", user);
+        id = user.getId();
+        sqlSession.commit();
+        sqlSession.close();
+        Assert.assertEquals(12, id);
+    }
+
+    @Test
+    public void TestFuzzySearch() throws IOException {
+        List<User> userList = sqlSession.selectList("test.findUsersByUsername", "丽");
+        for (User user : userList) {
+            System.out.println("姓名:" + user.getUsername());
+            System.out.println("性别:" + user.getGender());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            System.out.println("生日:" + sdf.format(user.getBirthday()));
+            System.out.println("所在地:" + user.getProvince() + user.getCity());
+        }
+    }
+
+    @Test
+    public void TestDelete() {
+        int flag;
+        flag = sqlSession.delete("test.deleteUser", 10);
+        sqlSession.commit();
+        sqlSession.close();
+        Assert.assertEquals(1, flag);
+    }
+
+    @Test
+    public void TestUpdateName() {
+        int flag;
+        User user = new User();
+        user.setId(11);
+        user.setUsername("updateName");
+        flag = sqlSession.update("test.updateUserName", user);
+        sqlSession.commit();
+        sqlSession.close();
+        Assert.assertEquals(1, flag);
+    }
+
 
     @Test
     public void TestIdeaGit() {
